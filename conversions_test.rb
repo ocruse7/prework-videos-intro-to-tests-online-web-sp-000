1c2c03c65c325ce59a8ae67987fe604f37d33556
Owen Cruse <ocruse7@gmail.com>
2020-10-25 20:16:41 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -9,8 +9,8 @@ else
 end
 
 puts "Given 1, it should return 28.3495"
-answer = ounces_to_grams(1)
-if answer == 28.3495
+answer = ounces_to_grams(2)
+if answer == 28.3495 * 2
   puts "Passed!"
 else
   puts "Test failed: got #{answer} instead"
